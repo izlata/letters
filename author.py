d693411d56fc9cd11130714ae486fbe42e837088
Zlata <65206729+izlata@users.noreply.github.com>
2021-07-30 18:59:30 -0400

    Add files

---
+++ author.py
@@ -0,0 +1,7 @@
+class Author():
+    def __init__(self, name_id, name, info, photo, link):
+        self.name_id = name_id
+        self.name = name
+        self.info = info
+        self.photo = photo
+        self.link = link

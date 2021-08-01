904f65645bd3f902436a15ce97ae398726e64e42
Zlata <65206729+izlata@users.noreply.github.com>
2021-08-01 15:56:55 -0400

    Update author.py

--- author.py
+++ author.py
@@ -1,6 +1,6 @@
 class Author():
-    def __init__(self, name_id, name, info, photo, link):
-        self.name_id = name_id
+    def __init__(self, author_id, name, info, photo, link):
+        self.author_id = author_id
         self.name = name
         self.info = info
         self.photo = photo

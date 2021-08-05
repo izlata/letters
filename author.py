9faad10428f8e1f6f6985064bc4ba2087ce0acf4
Zlata <65206729+izlata@users.noreply.github.com>
2021-08-04 23:03:12 -0400

    Delete author.py

--- author.py
+++
@@ -1,7 +0,0 @@
-class Author():
-    def __init__(self, author_id, name, info, photo, link):
-        self.author_id = author_id
-        self.name = name
-        self.info = info
-        self.photo = photo
-        self.link = link

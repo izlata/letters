7f0ee6270ae20fa993064967c54ea60d35e9a235
Zlata <65206729+izlata@users.noreply.github.com>
2021-08-04 23:03:38 -0400

    Delete letter.py

--- letter.py
+++
@@ -1,7 +0,0 @@
-class Letter():
-    def __init__(self, letter_id, name_id, date, content, source):
-        self.letter_id = letter_id
-        self.name_id = name_id
-        self.date = date
-        self.content = content
-        self.source = source

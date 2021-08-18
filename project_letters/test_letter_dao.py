07db650b81bd89418868a073740142e10e75dc13
Zlata <zlata.izvalava@gmail.com>
2021-08-18 16:23:26 -0400

    Modify tests

--- project_letters/test_letter_dao.py
+++ project_letters/test_letter_dao.py
@@ -6,15 +6,24 @@ import unittest
 
 
 class TestLetterDao(unittest.TestCase):
-    def test_get_letter(self):
-        letter_dao = LetterDao(letters={1: Letter(1, 3, 'Hello'), 2: Letter(2, 1, 'ABCD')})
-        self.assertEqual(letter_dao.get_letter(1), Letter(1, 3, 'Hello'))
-        with self.assertRaises(KeyError):
-            letter_dao.get_letter(5)
+    def setUp(self):
+        self.dao = LetterDao(letters={1: Letter(letter_id=1, author_id=3, content='Hello'),
+                                      2: Letter(letter_id=2, author_id=3, content='ABCD')})
 
-    def test_create_letter(self):
-        letter_dao = LetterDao(letters={1: Letter(letter_id=1, author_id=3, content='Hello')})
-        letter_dao.create_letter(Letter(letter_id=0, author_id=3, content='ABCD'))
-        self.assertEqual(letter_dao.letters, {1: Letter(1, 3, 'Hello'), 2: Letter(2, 3, 'ABCD')})
+    def test_get_letter_existing_key(self):
+        self.assertEqual(self.dao.get_letter(1), Letter(1, 3, 'Hello'))
+
+    def test_get_letter_wrong_key(self):
+        with self.assertRaises(KeyError):
+            self.dao.get_letter(5)
+
+    def test_create_letter_new_record(self):
+        self.dao.create_letter(Letter(letter_id=0, author_id=3, content='Thank you'))
+        updated_dict = {1: Letter(1, 3, 'Hello'),
+                        2: Letter(2, 3, 'ABCD'),
+                        3: Letter(3, 3, 'Thank you')}
+        self.assertEqual(self.dao.letters, updated_dict)
+
+    def test_create_letter_existing_record(self):
         with self.assertRaises(ObjectAlreadyExistsError):
-            letter_dao.create_letter(Letter(letter_id=0, author_id=3, content='Hello'))
+            self.dao.create_letter(Letter(letter_id=0, author_id=3, content='Hello'))

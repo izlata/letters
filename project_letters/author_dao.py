daab701ceb61d1318e8540a55c043f7ae54cc72d
Zlata <zlata.izvalava@gmail.com>
2021-08-25 15:44:01 -0400

    Update author_dao.py

--- project_letters/author_dao.py
+++ project_letters/author_dao.py
@@ -8,10 +8,19 @@ class AuthorDao():
     def get_author(self, author_id):
         return self.authors[author_id]
 
-    def create_author(self, new_author):
-        if new_author in self.authors.values():
-            raise ObjectAlreadyExistsError
-        else:
-            new_id = len(self.authors) + 1
-            new_author.author_id = new_id
-            self.authors[new_id] = new_author
+    def save_author(self, author, author_id=None):
+        if author_id is None:  # create author
+            if author in self.authors.values():
+                raise ObjectAlreadyExistsError
+            else:
+                new_id = len(self.authors) + 1
+                author.author_id = new_id
+                self.authors[new_id] = author
+        else:  # update author
+            dict_copy = self.authors.copy()
+            del dict_copy[author_id]
+            if author in dict_copy.values():
+                raise ObjectAlreadyExistsError
+            else:
+                author.author_id = author_id
+                self.authors[author_id] = author
